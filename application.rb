6319020a1df7ec7a78489d5606e2aa3d2a408e41
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-05 19:52:45 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, my name is temitope Elodimuor"
+    resp.write "Hello, my name is temitope Elodimuor."
     resp.write "I am now learning the rack and rails was built on top of rack"
     resp.finish
   end
